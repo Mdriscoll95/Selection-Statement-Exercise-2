31c0f26c15dd7bc05014985b0dd32754bd01806d
Michael Driscoll <michael.driscoll951@gmail.com>
2023-05-16 07:30:40 -0700

    hw

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+            case "Math":
+                    Console.WriteLine("Math is awesome");
+               break;
+            case "science":
+                    Console.WriteLine("Ah I havent taken science before");
+                break;
+            case "history":
+                Console.WriteLine("I love history");
+                break;
+             case "French":
+                Console.WriteLine("c'est Bien");
+                break;
+             case "English":
+                Console.WriteLine("its alright");
+                break;
+             case "theatre arts":
+                Console.WriteLine("love it");
+                break;
+
+            }
+
+
         }
     }
 }
